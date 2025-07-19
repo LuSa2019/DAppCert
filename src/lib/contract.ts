c1d82064d244f2ad6356d6de642f0ef599e30367
LuSa2019 <luigisans.ls@gmail.com>
2025-07-19 16:39:44 +0200

    Inizio sviluppo dashboard enti

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -1,6 +1,6 @@
 // lib/contract.ts
 import { ethers } from "ethers";
-import abi from "@/abi/certificatiAbi.json";
+import abi from "@/abi/CertificatiAbi.json";
 
 const CONTRACT_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138"; // inserisci il tuo
 
