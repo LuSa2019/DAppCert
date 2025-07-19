a289f92663841471d66afcfb4d8879f3a8d0d6fb
LuSa2019 <luigisans.ls@gmail.com>
2025-07-19 17:21:27 +0200

    Aggiunto ABI file

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -1,10 +1,10 @@
 // lib/contract.ts
 import { ethers } from "ethers";
-import abi from "../abi/CertificatiAbi.json";
+import CertificatiABI from "../abi/CertificatiAbi.json";
 
 const CONTRACT_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138"; // inserisci il tuo
 
 // Con Magic o MetaMask collegato
 export function getContract(signerOrProvider: ethers.Signer | ethers.Provider) {
-  return new ethers.Contract(CONTRACT_ADDRESS, abi, signerOrProvider);
+  return new ethers.Contract(CONTRACT_ADDRESS, CertificatiABI, signerOrProvider);
 }
